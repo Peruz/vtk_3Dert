3e1b306c8aa4bbcead88df23388ee7581e8aa102
Peruz <lucaperuzzo@outlook.com>
2019-11-17 23:34:27 -0800

    test_data

---
+++ README.md
@@ -0,0 +1,12 @@
+# VTK plot script for ERT data
+
+The code contains some funcitons to plot 3D resistivity distribtuions from vtk files.
+So far, I could not get to work the funtions using the `if __name__ == "__main__":' because of some errors in the vtk wrapping; therefore the file cannot be used as module, copy-paste the function instead.
+
+The file runs with 2 positional arguments from command-line:
+* name of the vtk file (e.g., inversionResult.vtk)
+* name of the scalar to plot (e.g., rho)
+
+
+
+
--- vtk_plot.py
+++ vtk_plot.py
@@ -1,32 +1,33 @@
 from vtk import (vtkUnstructuredGridReader, vtkDataSetMapper, vtkActor,
                  vtkRenderer, vtkRenderWindow, vtkRenderWindowInteractor)
 import vtk
-import os
-
+import sys
+import argparse
 """
 
 reader(datafile) -> mapper(reader.GetOutputPort(), LookupTable) -> actor(mapper) -> render(actor1, ...) -> window(render)
 
 """
 
-def read_vtk(fileName, scalarName):
+def read_vtk(fileName):
     reader = vtkUnstructuredGridReader()
     reader.SetFileName(fileName)
-    reader.SetScalarsName(scalarName)
+    reader.ReadAllScalarsOn()
+    #reader.ReadAllVectorsOn()
     reader.Update()  # Needed because of GetScalarRange
     return(reader)
-    
+
 def lookup_table(output):
-    scalar_range = output.GetScalarRange()
     lut = vtk.vtkLookupTable()
-    #lut.SetTableRange(scalar_range)
-    lut.SetTableRange(30, 500)
-    lut.SetNumberOfColors(50)
-    lut.SetHueRange(0.667, 0.0)
+    scalar_range = output.GetScalarRange()
+    lut.SetTableRange(scalar_range)
+#    lut.SetTableRange(10, 35)
+    lut.SetHueRange(0.667, 0)
+    lut.SetSaturationRange (0.7, 0.7)
     lut.SetNumberOfColors(256)
     lut.Build()
     return(lut)
-    
+
 def data_actor(output_port, lut):
     data_mapper = vtkDataSetMapper()
     data_mapper.SetInputConnection(output_port)
@@ -54,7 +55,12 @@ def axes2D_actor(output_port, renderer):
     cubeAxes_actor.SetZAxisVisibility(0)
     cubeAxes_actor.SetYAxisVisibility(1)
     cubeAxes_actor.SetXAxisVisibility(1)
-    cubeAxes_actor.SetFontFactor(1.5)
+    cubeAxes_actor.SetFontFactor(1.2)
+    cubeAxes_actor.GetXAxisActor2D().GetProperty().SetLineWidth(0.8)
+    cubeAxes_actor.GetYAxisActor2D().GetProperty().SetLineWidth(0.8)
+    cubeAxes_actor.GetZAxisActor2D().GetProperty().SetLineStippleRepeatFactor(20)
+    cubeAxes_actor.GetYAxisActor2D().SetTickLength(1)
+
     return(cubeAxes_actor)
 
 def axes_actor(output_port, renderer, reader):
@@ -85,7 +91,7 @@ def axes_actor(output_port, renderer, reader):
     cubeAxesActor.GetLabelTextProperty(1).SetColor(axis2Color)
     cubeAxesActor.GetLabelTextProperty(2).SetFontSize(fontsize)
     cubeAxesActor.GetTitleTextProperty(2).SetFontSize(fontsize)
-    
+
     cubeAxesActor.GetTitleTextProperty(2).SetColor(axis3Color)
     cubeAxesActor.GetLabelTextProperty(2).SetColor(axis3Color)
     cubeAxesActor.SetLabelOffset(20)  # default 20
@@ -100,7 +106,7 @@ def axes_actor(output_port, renderer, reader):
     cubeAxesActor.SetYLabelFormat("%2.2g")
     cubeAxesActor.SetZLabelFormat("%2.2g")
     cubeAxesActor.SetYAxisTickVisibility(0)
-    cubeAxesActor.SetTitleScaling(7, 3,3,3)
+    #cubeAxesActor.SetTitleScaling(7, 3,3,3)
     #cubeAxesActor.SetScale(1,1,0.1)
     # cubeAxesActor.SetPosition(10, 0, 0)
     return(cubeAxesActor)
@@ -108,15 +114,16 @@ def axes_actor(output_port, renderer, reader):
 def colorbar_actor(lut):
     colorbar = vtk.vtkScalarBarActor()
     colorbar.SetLookupTable(lut)
-    colorbar.SetWidth(0.12)
+    colorbar.SetWidth(0.08)
     colorbar.SetBarRatio(0.5)
     colorbar.SetHeight(0.7)
     colorbar.SetPosition(0.8, 0.1)
-    colorbar.SetLabelFormat("%.3g")
+    colorbar.SetLabelFormat("%4.3g")
     colorbar.PickableOff()
     #colorbar.VisibilityOn()
-    colorbar.SetTitle("Rho \n [Ohm m]\n\n")
-    colorbar.SetNumberOfLabels(10)
+    colorbar.SetTitle("Rho \nOhm m\n")
+    colorbar.SetNumberOfLabels(5)
+    #colorbar.SetTextPosition()
     colorbar.GetLabelTextProperty().SetColor(0,0,0)
     colorbar.GetLabelTextProperty().SetFontSize(10)
     colorbar.GetTitleTextProperty().SetColor(0,0,0)
@@ -128,8 +135,8 @@ def colorbar_actor(lut):
     return(colorbar)
 
 def renderer_options(renderer):
-    renderer.SetBackground(0.9, 0.9, 0.9) # Set background to white
-    renderer.GetActiveCamera().Azimuth(0)
+    renderer.SetBackground(1, 1, 1) # Set background to white
+    renderer.GetActiveCamera().Azimuth(30)
     renderer.GetActiveCamera().Elevation(20)
     renderer.GetActiveCamera().Zoom(0.9)
     # renderer.GetActiveCamera().SetViewUp(0,0,0.5)
@@ -138,7 +145,7 @@ def renderer_options(renderer):
     # renderer.GetActiveCamera().SetPosition(0, 1, 0.2)
     renderer.ResetCamera()
     return(renderer)
-    
+
 def renderer_window(renderer):
     renderer_window = vtkRenderWindow()
     renderer_window.AddRenderer(renderer)
@@ -146,16 +153,16 @@ def renderer_window(renderer):
     return(renderer_window)
 
 def save_png(renderer_window, nameout):
-    """ renderer_window -> WindowToImageFilter -> Writer -> png """ 
+    """ renderer_window -> WindowToImageFilter -> Writer -> png """
     w2if = vtk.vtkWindowToImageFilter()
     w2if.SetInput(renderer_window)
     w2if.Update()
-    
+
     writer = vtk.vtkPNGWriter()
     writer.SetFileName(nameout)
     writer.SetInputData(w2if.GetOutput())
     writer.Write()
-    
+
 def interactor(renderer_window):
     """ Create the RendererWindowInteractor and display the vtk_file """
     interactor = vtkRenderWindowInteractor()
@@ -163,29 +170,33 @@ def interactor(renderer_window):
     interactor.Initialize()
     interactor.Start()
 
+def arguments():
+    parser = argparse.ArgumentParser(description='plot vtk file')
+    parser.add_argument('file_name', type=str)
+    parser.add_argument('scalar_name', type=str)
+    args = parser.parse_args()
+    return(args.file_name, args.scalar_name)
 
-files = [f for f in os.listdir() if (f.endswith('.vtk') and '2018' in f)]
-for f in files:
-    print(f)
-    reader = read_vtk(f, 'res')
-    output = reader.GetOutput()
-    output_port = reader.GetOutputPort()
+f, s = arguments()
+reader = read_vtk(f)
+output = reader.GetOutput()
+output_port = reader.GetOutputPort()
+output.GetCellData().SetActiveScalars(s)
+rr = vtkRenderer()
 
-    rr = vtkRenderer()
+lut = lookup_table(output)
+data_act = data_actor(output_port, lut)
+axes_act = axes2D_actor(output_port, rr)
+colorbar_act = colorbar_actor(lut)
 
-    lut = lookup_table(output)
-    data_act = data_actor(output_port, lut)
-    #axes_act = axes2D_actor(output_port, rr)
-    axes_act = axes_actor(output_port, rr, reader)
-    colorbar_act = colorbar_actor(lut)
-    
-    rr.AddActor(data_act)
-    rr.AddActor(colorbar_act)
-    rr.AddActor(axes_act)
-    
-    rr = renderer_options(rr)
-    rw = renderer_window(rr)
-    
-    nameout = f.replace('.vtk', '_ResVTK.png')
-    save_png(rw, nameout)
-    interactor(rw)
+rr.AddActor(data_act)
+rr.AddActor(colorbar_act)
+rr.AddActor(axes_act)
+
+rr = renderer_options(rr)
+rw = renderer_window(rr)
+
+
+nameout = f.replace('.vtk', '_ResVTK.png')
+save_png(rw, nameout)
+#interactor(rw)
